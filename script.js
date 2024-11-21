da7280c1aa36eee0a065cae3bb272f55c83a1661
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-22 03:05:09 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Generate computers  choice randomly.
+// Ask user for input, check if user input is either 'rock', 'paper' 'or' 'scissors'
+// Logic for one round of play and declare result
+// Logic for multiple rounds of play, number of rounds provided by the user.
