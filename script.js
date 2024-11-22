0cab70a822fc5956bff5b0e07e1d9a65f2dd6ec7
Sid2169 <siddharthasingh21109572@gmail.com>
2024-11-22 06:12:01 +0530

    Version 1 fianl Commit
    This is the final commit  of  version 1. Fixed minor bugs prototype. Ready with intitial minimum specfication of this project.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function computerChoice() {
     let choice = Math.random() * 3;
     if (choice > 1) choice = 'rock';
-    else if(choice >= 1 && choice > 2) choice = 'paper';
+    else if (choice >= 1 && choice > 2) choice = 'paper';
     else choice = 'scissor';
     return choice;
 }
@@ -10,33 +10,32 @@ function computerChoice() {
 // Ask user for input, check if user input is either 'rock', 'paper' 'or' 'scissors'
 function gethumanChoice() {
     let choice = prompt("What is your choice?").toLowerCase();
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissor') {
-        console.log("Please provide a valid choice between 'rock' 'paper' and 'scissor'. Start the game again \n");
-        return '';
+    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissor') {
+        prompt("Please provide a valid choice between 'rock' 'paper' and 'scissor'.").toLowerCase();
     }
     return choice;
 }
 // Logic for one round of play and declare result
 function playRound(computer, human) {
-    
-    computer.toLowerCase();
-    human.toLowerCase();
-    if (computer !== 'rock' && playe1 !== 'paper' && computer !== 'scissor'
-    && human !== 'rock' && playe2 !== 'paper' && human !== 'scissor') {
+
+    computer = computer.toLowerCase();
+    human = human.toLowerCase();
+    if (computer !== 'rock' && computer !== 'paper' && computer !== 'scissor'
+        && human !== 'rock' && human !== 'paper' && human !== 'scissor') {
         console.log('Invalid choice by player/players');
         return 'end'
     }
 
     if (computer === human) return 'draw';
-    
+
     else if ((computer === 'rock' && human === 'scissor') ||
-             (computer === 'paper' && human === 'rock') ||
-             (computer === 'scissor' && human === 'paper')) {
+        (computer === 'paper' && human === 'rock') ||
+        (computer === 'scissor' && human === 'paper')) {
         return 'computer';
-    } 
+    }
 
     else return 'human';
-}    
+}
 // Logic for multiple rounds of play, number of rounds provided by the user
 function playGame() {
     let rounds = Number(prompt("How many rounds you want to play?"));
@@ -47,26 +46,31 @@ function playGame() {
     let computerScore = 0;
     let humanScore = 0;
     let roundCounter = 0;
-    while (roundCounter > rounds)
-    {
+    while (roundCounter < rounds) {
         let computer = computerChoice();
         let human = gethumanChoice();
-        result = playRound(computer, human)
+        let result = playRound(computer, human)
+        console.log(`Round ${roundCounter}: Computer: ${computer}   You: ${human}       `);
+
         if (result === 'end') {
             break;
         }
-        if(result === 'computer') {
+        else if (result === 'computer') {
             computerScore++;
             console.log('You lose this round! Compuer gains 1 point\n')
         }
-        if (result === 'human') {
+        else if (result === 'human') {
             humanScore++;
             console.log('You Win this round! You gain 1 point\n')
         }
+        else {
+            console.log('This round is draw\n')
+        }
+        roundCounter++;
     }
     console.log('Score Board :-\n Computer: ' + computerScore + ' ' + 'You: ' + humanScore + '\n');
     if (computerScore === humanScore) console.log('The game was a draw');
-    if (computerScore > humanScore) console.log('You lost! Better luck next time');
+    else if (computerScore > humanScore) console.log('You lost! Better luck next time');
     else console.log("You win! Congratulations");
 
 }
